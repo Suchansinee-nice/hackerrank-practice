202be07a00368362c3d5bc2b846d0ac45fcfd575
Suchansinee_nice <schsn.nice@gmail.com>
2023-06-23 16:42:20 +0700

    commit

---
+++ project/src/project/SubS.java
@@ -0,0 +1,24 @@
+package project;
+
+import java.util.Scanner;
+
+public class SubS {
+	
+	//https://www.hackerrank.com/challenges/java-substring/problem
+	
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter string : ");
+        String S = in.next();
+        System.out.println("Enter start pos : ");
+        int start = in.nextInt();
+        System.out.println("Enter end pos : ");
+        int end = in.nextInt();
+        
+      
+        String substring = S.substring(start, end);
+        System.out.println(substring);
+
+    }
+
+}
